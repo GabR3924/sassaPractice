0427b8732196c96ce37ba1ae4afcabf47b58b577
GabR3924 <garc2004@gmail.com>
2024-02-16 17:41:17 -0400

    36:48

--- src/Provider/Provider.jsx
+++ src/Provider/Provider.jsx
@@ -1,12 +1,44 @@
-import { createContext } from "react"; 
+import { v4 as uuidv4 } from "uuid";
+import { createContext } from "react";
 
-const bbdd = {}
+const bbdd = {
+    header : {
 
-export const GlobalContext = createContext(bbdd)
+        menu : [
+            { id : uuidv4() , title : "Home"         , href :"#"},
+            { id : uuidv4() , title : "Movies "      , href :"#"},
+            { id : uuidv4() , title : "Games"        , href :"#"},
+            { id : uuidv4() , title : "Experiences " , href :"#"},
+            { id : uuidv4() , title : "Experiences"  , href :"#"},
+            { id : uuidv4() , title : "Community"    , href :"#"},
+            { id : uuidv4() , title : "Publishing"   , href :"#"},
+            { id : uuidv4() , title : "Partnerships" , href :"#"},
+            { id : uuidv4() , title : "Our Team"     , href :"#"},
+        ] ,
+        rrss : [
+            { id : uuidv4() , title : 'Facebook'  , href : '#' , icono : 'facebook'},
+            { id : uuidv4() , title : 'Twitter'   , href : '#' , icono : 'twitter'},
+            { id : uuidv4() , title : 'Instagram' , href : '#' , icono : 'instagram'},
+            { id : uuidv4() , title : 'Youtube'   , href : '#' , icono : 'youtube'},
+        ]
+    },
+    hero : {
+        h2 : 'Avatar the way of water',
+        claim : 'Watch the brand-new trailer and experience Avatar: The Way of Water in 3D on December 16.'
+        ,
+        buttons : [
+            { id : uuidv4() , title : 'Get tickets'   , href : '#'},
+            { id : uuidv4() , title : 'Watch Trailer' , href : '#'},
+        ]
+    }
+};
+
+export const GlobalContext = createContext(bbdd);
 
 export const Provider = ({ children }) => {
-    <GlobalContext.Provider value={ bbdd }>
-        { children }
+  return (
+    <GlobalContext.Provider value={bbdd}>
+        {children}
     </GlobalContext.Provider>
-
-}
+  );
+};
