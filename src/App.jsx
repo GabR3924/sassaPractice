87d6ebdb9638317cb38ad695f3e4b695ff71eafc
GabR3924 <garc2004@gmail.com>
2024-02-17 12:55:58 -0400

    sad

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // import { Header } from './components/Header/Header'
 // import { Hero } from './components/Hero/Hero'
 import './App.scss'
+import Header from './Header/Header'
 
 function App() {
 
   return (
     <div className='App'>
       avatar
+      <Header/>
     </div>
   )
 }
