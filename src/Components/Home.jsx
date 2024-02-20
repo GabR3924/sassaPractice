fabadd8fcec45cf6c0187700da29af7808a407b9
GabR3924 <garc2004@gmail.com>
2024-02-20 12:41:37 -0400

    salvation

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -18,9 +18,11 @@ function Home() {
           <img src={home} alt="home image" />
         </div>
         <div className="ellipse-container">
+          <div className="ellipse">
           <div className="ellipse pink"></div>
           <div className="ellipse orange"></div>
         </div>
+          </div>
       </div>
     </div>
   </div>
