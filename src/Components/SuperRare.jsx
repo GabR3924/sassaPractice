7fa4546842e8a8147d1b6f33fbb640d4b63f212e
GabR3924 <garc2004@gmail.com>
2024-02-21 13:23:18 -0400

    ll

--- src/Components/SuperRare.jsx
+++ src/Components/SuperRare.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import super1 from '.../assets.super1.png'
-import super2 from '.../assets.super2.png'
-import super3 from '.../assets.super3.png'
-import super4 from '.../assets.super4.png'
+import super1 from '../assets/super1.png'
+import super2 from '../assets/super2.png'
+import super3 from '../assets/super3.png'
+import super4 from '../assets/super4.png'
+import Card from './Card'
 
 function SuperRare() {
   const data = [
@@ -14,21 +15,21 @@ function SuperRare() {
       tag:12345,
       time: 1,
     },{
-      image: super1,
+      image: super2,
       series: "Gloop Series",
       title:"purple Man",
       price: 2.99,
       tag:12345,
       time: 1,
     },{
-      image: super1,
+      image: super3,
       series: "Gloop Series",
       title:"purple Man",
       price: 2.99,
       tag:12345,
       time: 1,
     },{
-      image: super1,
+      image: super4,
       series: "Gloop Series",
       title:"purple Man",
       price: 2.99,
@@ -36,9 +37,33 @@ function SuperRare() {
       time: 1,
     },
   ]
-  
+
   return (
-    <div>SuperRare</div>
+    <div className='super-rare'>
+      <div className="title-container">
+        <h2 className="title">LE Super Rare Auction</h2>
+        <p className="description">Lorem, ipsum dolor sit amet consectetur 
+         ven sunt sit obcaecati assumenda, ab, nemo officiis
+         <a href="#">OpenSea</a>
+        </p>
+      </div>
+
+      <div className="cards">
+        {
+          data.map(({ image, series, title, price, tag, time }, index ) => 
+          <Card
+          image= {image}
+          series= {series}
+          title={title}
+          price= {price}
+          tag={tag}
+          time= {time}
+          key={index}
+          />
+          )
+        }
+      </div>
+    </div>
   )
 }
 
